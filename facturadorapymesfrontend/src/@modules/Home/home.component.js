3239b07e5517aa7f514881f797d15fc1100580a0
Dayana Silva Gaitán <Dsilva@intergrupo.com>
2021-07-23 15:31:55 -0500

    Adelanto 23-07-2021

---
+++ facturadorapymesfrontend/src/@modules/Home/home.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class Home extends React.Component{
+
+    render(){
+        return(
+            <div class="container">
+                <h1>hola</h1>
+            </div>
+        );
+    }
+}
---
+++ facturadorapymesfrontend/src/@modules/NotFound/notFound.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class NotFound extends React.Component{
+
+    render(){
+        return(
+            <div class="container">
+                <h1>pagina no encontrada</h1>
+            </div>
+        );
+    }
+}
